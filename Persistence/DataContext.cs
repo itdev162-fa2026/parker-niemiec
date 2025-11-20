b6e94423b38505ffb7af9ac893b41833dc24ae9c
Parker Niemiec <niemiecp@matc.edu>
2025-11-19 19:21:00 -0600

    not working

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,29 +1,38 @@
 using Domain;
 using Microsoft.EntityFrameworkCore;
 
-namespace Persistence;
-
-public class DataContext : DbContext
+namespace Persistence
 {
+  public class DataContext : DbContext
+  {
     public DbSet<WeatherForecast> WeatherForecasts { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderItem> OrderItems { get; set; }
+    public string DbPath { get; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public DataContext()
     {
-        optionsBuilder.UseSqlite("Data Source=blogbox.db");
+      var folder = Environment.SpecialFolder.LocalApplicationData;
+      var path = Environment.GetFolderPath(folder);
+      DbPath = System.IO.Path.Join(path, "Blogbox.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+      options.UseSqlite($"Data Source={DbPath}");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+      base.OnModelCreating(modelBuilder);
 
-        // Configure Order-OrderItem relationship
-        modelBuilder.Entity<Order>()
-            .HasMany(o => o.OrderItems)
-            .WithOne(oi => oi.Order)
-            .HasForeignKey(oi => oi.OrderId)
-            .OnDelete(DeleteBehavior.Cascade);
+      // Configure Order-OrderItem relationship
+      modelBuilder.Entity<Order>()
+        .HasMany(o => o.OrderItems)
+        .WithOne(oi => oi.Order)
+        .HasForeignKey(oi => oi.OrderId)
+        .OnDelete(DeleteBehavior.Cascade);
     }
+  }
 }
